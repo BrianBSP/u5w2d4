20c360b0de1e8b50f270f36b58ad904635fb4169
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-09-13 02:37:01 +0200

    implemento serverConfig

---
+++ src/main/java/brianpelinku/u5w2d4/config/ServerConfig.java
@@ -0,0 +1,14 @@
+package brianpelinku.u5w2d4.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig {
+
+    @Bean
+    public Cloudinary imageUpload(){
+
+    }
+}
